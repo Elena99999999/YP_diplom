fbd0f4d8a16879e32ee81a6f055bf2a625852e6f
Elena99999999 <157041158+Elena99999999@users.noreply.github.com>
2024-01-18 20:56:59 +0300

    data.py

---
+++ data.py
@@ -0,0 +1,19 @@
+headers = {
+
+   "Content-Type": "application/json"
+
+}
+
+create_order = {
+    "firstName": "Борнов",
+    "lastName": "Тим",
+    "address": "5",
+    "metroStation": 8,
+    "phone": "+78559665477",
+    "rentTime": 2,
+    "deliveryDate": "2024-01-18",
+    "comment": "Во время",
+    "color": [
+       "BLACK"
+             ]
+}
